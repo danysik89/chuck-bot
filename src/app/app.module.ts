b260398947b621b1a30bfff4be6172cb5748e908
Lenovo-PC\Danylo <danilogoryachko@gmail.com>
2020-06-01 02:59:14 +0300

    chat bot functional and template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChatModule } from './chat/chat.module';
+import { ChatHomeComponent } from './chat/chat-home/chat-home.component';
+import { ChatDialogComponent } from './chat/chat-dialog/chat-dialog.component';
 
+const appRoutes: Routes = [
+  {path: '', component: ChatHomeComponent},
+  {path: ':id', component: ChatDialogComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    ChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chat/chat.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+export class Message {
+  constructor(public content: string, public sentBy: string, date: any) {}
+}
+
+@Injectable()
+export class ChatService {
+
+  conversations = [
+    {
+      id: 0,
+      name: 'Leonid Teriohin',
+      conversation: new BehaviorSubject([])
+    },
+    {
+      id: 1,
+      name: 'Ostap Ferneza',
+      conversation: new BehaviorSubject([])
+    },
+    {
+      id: 2,
+      name: 'Vlad Darmohray',
+      conversation: new BehaviorSubject([])
+    },
+    {
+      id: 3,
+      name: 'Accorn Dobrovolskiy',
+      conversation: new BehaviorSubject([])
+    },
+  ];
+
+  constructor(private http: HttpClient) { }
+
+  update(id: number, msg: Message) {
+    const currentContact = this.conversations.find(user => user.id === id);
+    currentContact.conversation.next(currentContact.conversation.getValue().concat([msg]));
+  }
+
+  converse(id: number, msg: string) {
+    const userMessage = new Message(msg, 'user', new Date());
+    this.update(id, userMessage);
+
+    return this.http.get('https://api.chucknorris.io/jokes/random')
+      .subscribe((response: any) => {
+        const botMessage = new Message(response.value, 'bot', new Date());
+        this.update(id, botMessage);
+      });
+  }
+
+}
